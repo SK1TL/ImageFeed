52460e22785585e6a4c4c421acfc25cd3b05c140
Artur <SSSK1TLLL@yandex.ru>
2024-01-19 18:01:57 +0300

    создан ProfileViewController и IBOutlet

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Артур Гайфуллин on 19.01.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    
+    @IBAction func didTapLogoutButton() {
+    }
+}
