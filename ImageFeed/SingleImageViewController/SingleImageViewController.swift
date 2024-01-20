87ea113819542c8c5a0fae63feb292333d2964ed
Artur <SSSK1TLLL@yandex.ru>
2024-01-20 15:41:50 +0300

    SingleImageView

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Артур Гайфуллин on 20.01.2024.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet private var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
