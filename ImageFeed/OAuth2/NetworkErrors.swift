cfe754deb0e5c9a16cc1724768b5243039f18805
Artur <sssk1tlll@yandex.ru>
2024-02-19 13:47:48 +0300

    +

--- ImageFeed/OAuth2/NetworkErrors.swift
+++ ImageFeed/OAuth2/NetworkErrors.swift
@@ -7,8 +7,52 @@
 
 import Foundation
 
-enum NetworkErrors: Error {
-    case invalidURL
-    case httpStatusCode(Int)
-    case invalidDecoding
+extension URLSession {
+    func objectTask<T: Decodable>(
+        for request: URLRequest,
+        completion: @escaping (Result<T, Error>) -> Void
+    ) -> URLSessionTask {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data,
+               let response = response,
+               let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200..<300 ~= statusCode {
+                    let decoder = JSONDecoder()
+                    decoder.keyDecodingStrategy = .convertFromSnakeCase
+                    guard let object = try? decoder.decode(T.self, from: data) else {
+                        completion(.failure(NetworkErrors.invalidDecoding))
+                        return
+                    }
+                    completion(.success(object))
+                }
+                else {
+                    completion(.failure(error ?? NetworkErrors.httpStatusCode(statusCode)))
+                    return
+                }
+            }
+        }
+        return task
+    }
+    
+    enum NetworkErrors: Error {
+        case invalidUrl
+        case httpStatusCode(Int)
+        case invalidDecoding
+    }
+}
+
+extension URLRequest {
+    static func makeHTTPRequest(
+        path: String,
+        httpMethod: String,
+        baseURL: String = Constants.defaultBaseURL.rawValue
+    ) -> URLRequest {
+        guard
+            let baseURL = URL(string: baseURL),
+            let url = URL(string: path, relativeTo: baseURL)
+        else {fatalError("Failed to create URL")}
+        var request = URLRequest(url: url)
+        request.httpMethod = httpMethod
+        return request
+    }
 }
